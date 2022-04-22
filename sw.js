81e56741131e20289a37a41651a97ab391e9b564
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-04-22 10:55:34 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,57 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.2/workbox-sw.js');
+var CACHE_NAME = 'v2';
 
-workbox.routing.registerRoute(
-  ({request}) => request.destination === 'image',
-  new workbox.strategies.NetworkFirst()
-);
-   
+var urlsToCache = [
+];
+
+self.addEventListener('install', function (event) {
+   // Perform install steps
+   event.waitUntil(
+      caches.open(CACHE_NAME)
+         .then(function (cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+         }).then(() => {
+            console.log('Cache downloaded')
+            self.skipWaiting()
+         })
+   );
+});
+
+
+
+self.addEventListener('fetch', function (event) {
+   event.respondWith(
+       caches.match(event.request)
+           .then(function (response) {
+               // Cache hit - return response
+               if (response) {
+                   return response;
+               }
+               console.log('cache miss', event.request.url)
+               return fetch(event.request);
+           })
+   );
+});
+
+
+self.addEventListener('activate', function (event) {
+   console.log('activated, remove unused cache...')
+   var cacheAllowlist = [CACHE_NAME];
+   event.waitUntil(
+       caches.keys().then(function (cacheNames) {
+           return Promise.all(
+               cacheNames.map(function (cacheName) {
+                   if (cacheAllowlist.indexOf(cacheName) === -1) {
+                 console.log(cacheName)
+                       return caches.delete(cacheName);
+                   }
+               })
+           );
+       })
+   );
+  clients.matchAll({includeUncontrolled:true,type:'window'}).then((arr) => {
+     for (client of arr) {
+        client.postMessage({type:'updated'})
+     }
+  })
+});
